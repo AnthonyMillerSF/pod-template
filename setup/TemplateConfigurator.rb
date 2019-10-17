9937c1b011158a5cfbfb28b5e40f609f7b0d93bd
Anthony Miller <anthony.miller@salesforce.com>
2019-10-17 13:10:58 -0700

    Take 3

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -155,12 +155,16 @@ module Pod
         test_mocks_podspec_file = File.readlines test_mocks_podspec_path
 
         new_line_index = -1
-        test_mocks_podspec_file.each_with_index { |line, index| new_line_index = index + 1 if line.include? "s.dependency 'PC" }
+        test_mocks_podspec_file.each_with_index do |line, index| 
+          if line.include? "s.dependency 'PC"
+            new_line_index = index + 1 
+          end
+        end
         
         if new_line_index != -1
           test_mocks_podspec_file.insert(new_line_index, "s.dependency '" + @pod_name + "'" 
         end
-        
+
         File.write(test_mocks_podspec_path, test_mocks_podspec_file)
     end
 
